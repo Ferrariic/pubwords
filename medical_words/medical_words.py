ab41556d0a8f39befa33f4aafe96c38fb77278fc
Ferrariic <thearcticfalcon@gmail.com>
2022-02-15 21:42:40 -0500

    rename files

--- medical_words/medical_words.py
+++ medical_words/medical_words.py
@@ -3,7 +3,7 @@ def medical_words() -> list:
     Returns:
         list: A list of 19,000 medical words from scraped publically available PubMed abstracts.
     """
-    with open('src\\medical_words.txt', 'r') as med:
+    with open('medical_words\\medical_words.txt', 'r') as med:
         file_contents = med.read()
     med_words = file_contents.split('\n')
     return med_words
