24c1ba414a2376f4f52336492f7574420fce6fff
Ferrariic <thearcticfalcon@gmail.com>
2022-02-15 21:18:06 -0500

    cleans up files

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Medical-Words
+
+This is a collection of 19,000 words scraped from publically available PubMed abstracts. 
---
+++ src/medical_words.py
@@ -0,0 +1,9 @@
+def medical_words() -> list:
+    """
+    Returns:
+        list: A list of 19,000 medical words from scraped publically available PubMed abstracts.
+    """
+    with open('src\\medical_words.txt', 'r') as med:
+        file_contents = med.read()
+    med_words = file_contents.split('\n')
+    return med_words
